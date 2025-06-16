5cb50ed5fa55e9ef793c270cbceedbad587edb2c
TroJanRuLes <andrey321861@gmail.com>
2025-06-17 00:11:59 +0300

    commit

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-
-import gameLogic from '../src/gameLogic.js';
 import runGame from '../src/runGame.js';
+import game from '../src/gameLogic.js';
 
-runGame(gameLogic.rules, gameLogic.generateQuestion);
+runGame(game);
