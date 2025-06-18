7151b986b4eb69fc5228b606debff832d4c683a5
TroJanRuLes <andrey321861@gmail.com>
2025-06-18 23:04:24 +0300

    commit

---
+++ src/games/even.js
@@ -0,0 +1,15 @@
+import getRandomNumber from '../utils.js';
+
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
+
+const generateRound = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const question = String(number);
+
+  return { question, correctAnswer };
+};
+
+export { rule, generateRound };
